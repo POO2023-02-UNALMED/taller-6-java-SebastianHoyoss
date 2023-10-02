f969a2ce91fd5da35b96aff5211f8dc077867b8a
SebastianHoyoss <jhoyosc@unal.edu.co>
2023-10-01 20:35:06 -0500

    metodos vehiculo

--- src/test/java/vehiculos/Vehiculo.java
+++ src/test/java/vehiculos/Vehiculo.java
@@ -24,4 +24,67 @@ public class Vehiculo {
 		this.fabricante=fabricante;
 		Vehiculo.cantidadVehiculos++;
 	}
+	public String getPlaca() {
+		return this.placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa=placa;
+	}
+	public int getPuertas() {
+		return this.puertas;
+	}
+	public void setPuertas(int puertas) {
+		this.puertas=puertas;
+	}
+	public int getVelocidadMaxima() {
+		return this.velocidadMaxima;
+	}
+	public void setVelocidadMaxima(int velmax) {
+		this.velocidadMaxima=velmax;
+	}
+	public String getNombre() {
+		return this.nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre=nombre;
+	}
+	public int getPrecio() {
+		return this.precio;
+	}
+	public void setPrecio(int precio) {
+		this.precio=precio;
+	}
+	public int getPeso() {
+		return this.peso;
+	}
+	public void setPeso(int peso) {
+		this.peso=peso;
+	}
+	public String getTraccion() {
+		return this.traccion;
+	}
+	public void setTraccion(String traccion) {
+		this.traccion=traccion;
+	}
+	public Fabricante getFabricante() {
+		return this.fabricante;
+	}
+	public void setFabricante(Fabricante fabricante) {
+		this.fabricante=fabricante;
+	}
+	public static int getCantidadVehiculos() {
+		return Vehiculo.cantidadVehiculos;
+	}
+	public static void setCantidadVehiculos(int vehiculos) {
+		Vehiculo.cantidadVehiculos=vehiculos;
+	}
+	public static ArrayList<Vehiculo> getVehiculos(){
+		return Vehiculo.vehiculos;
+	}
+	public static void agregarVehiculos(Vehiculo vehiculo) {
+		Vehiculo.vehiculos.add(vehiculo);
+	}
+	public static String vehiculosPorTipo() {
+		return "Automoviles: " + Automovil.getCantidadAutomoviles() + "\nCamionetas: " + Camioneta.getCantidadCamionetas() + "\nCamiones: " + Camion.getCantidadCamiones();
+	}
 }

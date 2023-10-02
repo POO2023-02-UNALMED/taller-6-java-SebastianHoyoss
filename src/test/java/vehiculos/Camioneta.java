a2cd3e192ab74bd2dead38725f3f59709c3348c7
SebastianHoyoss <jhoyosc@unal.edu.co>
2023-10-01 21:09:40 -0500

    metodos camion

--- src/test/java/vehiculos/Camioneta.java
+++ src/test/java/vehiculos/Camioneta.java
@@ -1,5 +1,22 @@
 package vehiculos;
 
 public class Camioneta extends Vehiculo{
-
+	private boolean volco;
+	private static int cantidadCamionetas;
+	
+	public Camioneta(String placa,int puertas,String nombre,int precio,int peso,Fabricante fabricante,boolean volco) {
+		super(placa,puertas,90,nombre,precio,peso,"4X4",fabricante);
+		this.volco=volco;
+		Camioneta.cantidadCamionetas++;
+		super.agregarVehiculos(this);
+	}
+	public boolean isVolco() {
+		return this.volco;
+	}
+	public void setVolco(boolean volco) {
+		this.volco=volco;
+	}
+	public static int getCantidadCamionetas() {
+		return Camioneta.cantidadCamionetas;
+	}
 }
